a7d932ddcb3d8d40b67d9be4a091982bc4db6dca
Fred Campos <mail2frede@gmail.com>
2020-08-12 15:06:16 -0300

    improved confs

--- snowman_challenge/config.py
+++ snowman_challenge/config.py
@@ -6,3 +6,4 @@ DATABASE_URL = environ.get('DATABASE_URL')
 HOST = environ.get('HOST', '0.0.0.0')
 PORT = environ.get('PORT', 8000)  # noqa: WPS432
 DEBUG = environ.get('DEBUG', True)
+LIMIT_ROWS = 20
