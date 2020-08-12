0167022e50d1a59f7e6db776f6da5d05bf5a6f19
Fred Campos <mail2frede@gmail.com>
2020-08-12 15:06:32 -0300

    improved tests confs

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ from snowman_challenge.database import db_engine, drop_db, init_db_session
 
 @pytest.fixture()
 def mixer() -> Generator:
-    """Test db session.
+    """Mixer fixture.
 
     :yield: Generator
     """
@@ -21,12 +21,38 @@ def mixer() -> Generator:
 
 
 @pytest.fixture()
+def mixer_with_spots() -> Generator:
+    """Mixer with spots fixture.
+
+    :yield: Generator
+    """
+    session = init_db_session()
+    mix = Mixer(session=session, commit=True)
+    spot1 = mix.blend(
+        'snowman_challenge.models.Spot',
+        name='Some Place 1',
+        category__name='some category 1',
+        user__username='email@example1.com',
+    )
+    spot1.save()
+    spot2 = mix.blend(
+        'snowman_challenge.models.Spot',
+        name='Some Place 2',
+        category__name='some category 2',
+        user__username='email@example2.com',
+    )
+    spot2.save()
+    yield mix
+    drop_db(db_engine)
+
+
+@pytest.fixture()
 def client() -> Generator:
     """Flask app fixture.
 
     :yield: Generator
     """
-    app = create_app('config')
+    app = create_app('snowman_challenge.config')
     app.config['TESTING'] = True
 
     with app.test_client() as cli:
