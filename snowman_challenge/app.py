004c0e639af30356b6df7d14be49e8a8c757ee5f
Fred Campos <mail2frede@gmail.com>
2020-08-12 15:06:04 -0300

    improved app bootstrap

--- snowman_challenge/app.py
+++ snowman_challenge/app.py
@@ -1,9 +1,13 @@
 """File containing flask app bootstrap.
 """
+from os import environ
+
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt import JWT
 
-
+from snowman_challenge.api.v1.views import blueprint
+from snowman_challenge.auth import authenticate, identify
 from snowman_challenge.database import init_db_session
 
 
@@ -14,8 +18,11 @@ def create_app(config_filename: str) -> Flask:
     :rtype: Flask
     :return: Flask app object
     """
-    app = Flask(__name__, static_folder='templates/static')
-    CORS(app)
-    app.config.from_object(config_filename)
+    flask_app = Flask(__name__, static_folder='templates/static')
+    flask_app.register_blueprint(blueprint, url_prefix='/api/v1')
+    CORS(flask_app)
+    flask_app.config.from_object(config_filename)
+    flask_app.config['SECRET_KEY'] = environ.get('JWT_SECRET_KEY', '')
+    JWT(flask_app, authenticate, identify)
     init_db_session()
-    return app
+    return flask_app
